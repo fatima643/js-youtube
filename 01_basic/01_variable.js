df8fe921a17fa7b8ed5a6f7c9725afe38c6a2efd
fatima643 <fatimasehar912@gmail.com>
2024-06-29 11:01:53 +0000

    container config done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,17 @@
+const accoundId= 12445
+let accountEmail="fatima@gmail.com"
+var accoundPassword="1234"
+accountyCity="faisalabad"
+
+
+
+
+// accoundId=2222
+console.log(accoundId)
+
+
+
+accountEmail="isha@gmail.com"
+accountPassword="2222"
+accountyCity="Multan"
+console.table( [accoundId,accountEmail,accountPassword,accountyCity])
