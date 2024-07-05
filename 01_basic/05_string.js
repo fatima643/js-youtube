7a8f7f27834964097f679049cbea038c87e2ff3b
fatima643 <fatimasehar912@gmail.com>
2024-07-05 12:43:54 +0000

    string

---
+++ 01_basic/05_string.js
@@ -0,0 +1,37 @@
+let name="fatima"
+let repAccount=60
+//console.log(name+repoAccount,"value")
+console.log(`hello my name is ${name} and my repoAccount is ${repAccount}`);
+
+const gameName= new String ("fatima-hc-c")
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length)
+// console.log(gameName.toLowerCase())
+// console.log(gameName.toLocaleUpperCase())
+// console.log(gameName.charAt(5))
+// console.log(gameName.indexOf('t'))
+const newString= gameName.substring(0,3)
+// console.log(newString);
+const AnotherString= gameName.slice(-3)
+// console.log(AnotherString);
+
+const newStringOne= "   fatima   "
+console.log(newStringOne);
+console.log(newStringOne.trim());// remove back and starting space
+
+const url="http://fatima.com/fatima%20sehar"
+
+console.log(url.replace("%20", "-"))
+console.log(url.includes('fatima'))
+console.log(gameName.split('-'))
+
+
+
+
+
+
+
+
+
+
